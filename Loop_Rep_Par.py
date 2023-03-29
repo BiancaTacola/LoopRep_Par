d21971e8bbaeec17699530a55ffeb7f91fa101a0
Bianca S. Tacola <95148673+BiancaTacola@users.noreply.github.com>
2023-03-29 14:54:09 -0300

    Add files via upload
    """Esse código é um loop de repetição que utiliza a função "range" para gerar uma sequência de números pares de 2 a 1000
    e, em seguida, imprime cada"""

---
+++ Loop_Rep_Par.py
@@ -0,0 +1,5 @@
+"""Esse código é um loop de repetição que utiliza a função "range" para gerar uma sequência de números pares de 2 a 1000
+e, em seguida, imprime cada"""
+
+for x in range(2,1001,2):
+    print(x)
